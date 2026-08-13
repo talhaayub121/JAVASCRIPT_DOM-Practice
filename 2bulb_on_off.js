0d0ec783dc46a6e6da80bd28e3532490849fc9c1
talhaayub121 <talha121@gmail.com>
2026-08-14 00:17:54 +0500

    DOM_Practice

---
+++ 2bulb_on_off.js
@@ -0,0 +1,9 @@
+var a=document.querySelector('.circle');
+var b=document.querySelector('.bt');
+b.addEventListener('click',function()
+{
+a.style.backgroundColor = "yellow";
+    a.style.boxShadow = "0 0 50px yellow";
+
+});
+console.log(b);
