34fc2ee51e7b0b8d9e84597337c2f1eb41e20a89
talhaayub121 <talha121@gmail.com>
2026-08-13 23:45:49 +0500

    DOM_Practice

---
+++ 1DOM.js
@@ -0,0 +1,8 @@
+var a=document.querySelector('.Name');
+a.addEventListener('click',function()
+{
+    a.innerHTML="My Name is Talha Ayub";
+    a.style.color="black";
+    a.style.background="yellow";
+})
+console.log(a);
